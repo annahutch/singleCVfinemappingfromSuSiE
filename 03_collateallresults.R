5516fb0b3434bda4fd7474d66751b54deeb749e9
Anna Hutchinson <42926930+annahutch@users.noreply.github.com>
2021-01-21 15:53:07 +0000

    Update 03_collateallresults.R

--- 03_collateallresults.R
+++ 03_collateallresults.R
@@ -1,14 +1,13 @@
 # script to collate the results from susie fine-mapping
 # and adjusted credible set fine-mapping for single CV regions
 
-# input: 
-# [1] file name of SuSiE output
+# input: file name of SuSiE output
 
 library(data.table)
 library(dplyr)
 
 args <- commandArgs(trailingOnly=TRUE)
-file <- args[1] # file with susie results
+file <- args # file with susie results
 
 susie_res <- fread(args)
 
@@ -20,19 +19,20 @@ susie_res_list <- split(susie_res_fm, susie_res_fm$block)
 
 ####################
 
-singleCVfm_files <- list.files(path = paste0("res_", file), pattern=".RDS", full=TRUE)
+singleCVfm_files <- list.files(path = "res", pattern=".RDS", full=TRUE)
 
 singleCVfm_list <- lapply(singleCVfm_files, readRDS)
+
+# extract second element from each list which is the full results
+singleCVfm_shortlist <- sapply(singleCVfm_list, "[[", 2, simplify = F)
 
 # remove from final list element 1 the single CV regions
 
 all_regions <- lapply(susie_res_list, function(x) x$block[1]) %>% unlist()
-singleCV_regions <- lapply(singleCVfm_list, function(x) x$block[1]) %>% unlist()
+singleCV_regions <- lapply(singleCVfm_shortlist, function(x) x$block[1]) %>% unlist()
 
 susie_res_only <- susie_res_list[-match(singleCV_regions, all_regions)]
 
 final_fm_res <- list(susie_res_only, singleCVfm_list)
 
 saveRDS(final_fm_res, paste0("finalFMres_", file, ".RDS"))
-
-
